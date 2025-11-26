709f6148f0df51267df310d2ff4b7c1515f0eea4
MKLIDE <145564237+MKLIDE@users.noreply.github.com>
2025-11-26 12:26:13 +0200

    Refactor deploy script to use hre for imports
    Updated import from 'hardhat' to 'hre' and improved logging message.

--- pge prototype/blockchain/scripts/deploy.js
+++ pge prototype/blockchain/scripts/deploy.js
@@ -1,17 +1,18 @@
-import hardhat from "hardhat"; // default import for CommonJS module
-const { ethers } = hardhat;
+import hre from "hardhat"; // default import for CommonJS module
 
 async function main() {
-  const GrowthOwnership = await ethers.getContractFactory("GrowthOwnership");
+  const GrowthOwnership = await hre.ethers.getContractFactory("GrowthOwnership");
+  // Deploy
   const contract = await GrowthOwnership.deploy();
-
+  //wait for deployment
   await contract.waitForDeployment();
 
-  console.log("Contract deployed to:", await contract.getAddress());
+  console.log("GrowthOwnership deployed to:", await contract.getAddress());
 }
 
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
 
